86366e3cab402661d3ce40dc86972b745034ab75
younes turki <younesturki@live.fr>
2020-05-17 16:23:04 +0200

    Refactoring and Creating Constant

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from 'src/app/list-todos/list-todos.component';
+import { API_URL } from 'src/app/app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -12,23 +13,23 @@ export class TodoDataService {
   ) { }
 
   retrieveAllTodos(username) {
-    return this.http.get<Todo[]>(`http://localhost:8585/users/${username}/todos`)
+    return this.http.get<Todo[]>(`${API_URL}/users/${username}/todos`)
     //console.log("Execute Hello World Bean Service")
   }
 
   deleteTodo(username, id){
-    return this.http.delete(`http://localhost:8585/users/${username}/todos/${id}`)
+    return this.http.delete(`${API_URL}/users/${username}/todos/${id}`)
   }
 
   retrieveTodo(username, id){
-    return this.http.get<Todo>(`http://localhost:8585/users/${username}/todos/${id}`)
+    return this.http.get<Todo>(`${API_URL}/users/${username}/todos/${id}`)
   }
-  
+
   updateTodo(username: string, id: number, todo: Todo) {
-    return this.http.put<Todo>(`http://localhost:8585/users/${username}/todos/${id}`, todo);
+    return this.http.put<Todo>(`${API_URL}/users/${username}/todos/${id}`, todo);
   }
 
   createTodo(username: string, todo: Todo) {
-    return this.http.post<Todo>(`http://localhost:8585/users/${username}/todos`, todo);
+    return this.http.post<Todo>(`${API_URL}/users/${username}/todos`, todo);
   }
 }
